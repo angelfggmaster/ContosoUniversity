d7f9f19f946d10ae5caa4aeebcfcb55bf91d48c2
Angel Graciano <angelfgg@msn.com>
2018-12-26 20:56:00 -0500

    Adición del modelo de datos y configuración de Web.config

---
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿namespace ContosoUniversity.DAL
+{
+    using Models;
+    using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
