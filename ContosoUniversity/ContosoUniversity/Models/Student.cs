49cbe35f163860b80dfc164b005d49b4dd2aedd7
BANCOLOMBIA\afgracia <afgracia@bancolombia.com.co>
2018-12-28 16:32:15 -0500

    Migración ColumnFirstName. DataAnnotations para la entidad Student

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -2,16 +2,37 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class Student
     {
         public int StudentID { get; set; }
 
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than {0} characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0}, {1}", this.LastName, this.FirstMidName);
+            }
+        }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
